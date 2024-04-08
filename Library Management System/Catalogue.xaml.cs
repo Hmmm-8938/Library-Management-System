792ae5ced9e76e6b515b1774887d98de2dacdea4
Benjamin Mellott <benamellott@gmail.com>
2024-04-08 12:13:18 -0600

    Updating Catalogue

--- Library Management System/Catalogue.xaml.cs
+++ Library Management System/Catalogue.xaml.cs
@@ -5,21 +5,23 @@ public partial class Catalogue : ContentPage
     public Catalogue()
     {
         InitializeComponent();
+        Book1Image.Source = "harry_potter_and_the_philosophers_stone_cover.jpg";
+        Book1Title.Text = "Title: Harry Potter and the Philosophers Stone";
+        Book1Author.Text = "Author: J.K Rowling";
+        Book1ISBN.Text = "ISBN: 1408855895";
+        //Book2Image.Source = "harry_potter_and_the_chamber_of_secrets_cover.jpg";
+        //Book2Title.Text = "Title: Harry Potter and the Chamber of Secrets";
+        //Book2Author.Text = "Author: J.K Rowling";
+        //Book2ISBN.Text = "ISBN: 1408855896";
     }
 
     private void Button_Clicked(object sender, EventArgs e)
     {
-        Book1Image.Source = "harry_potter_and_the_philosophers_stone_cover.jpg";
-        Book1Title.Text = "Harry Potter and the Philosopher's Stone";
-        Book1Author.Text = "J.K Rowling";
-        Book1ISBN.Text = "1408855895";
+        
     }
 
     private void Button_Clicked_1(object sender, EventArgs e)
     {
-        Book1Image.Source = "harry_potter_and_the_chamber_of_secrets_cover.jpg";
-        Book1Title.Text = "Harry Potter and the Chamber of Secrets";
-        Book1Author.Text = "J.K Rowling";
-        Book1ISBN.Text = "1408855896";
+        
     }
 }
