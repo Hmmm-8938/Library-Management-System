57956fa8abcdad49041c78509d8229b383f7088d
Jaxsimon <jaxsonburdett@gmail.com>
2024-04-12 16:30:45 -0600

    updated Create_User

--- Library Management System/CreateLibraryCard.xaml.cs
+++ Library Management System/CreateLibraryCard.xaml.cs
@@ -9,6 +9,13 @@ public partial class CreateLibraryCard : ContentPage
 
     private void Create_User(object sender, EventArgs e)
     {
-
+		//string userID = GetUserID();
+		//int PIN = GetPin();
+		string phoneNumberInput = phoneNumber.Text;
+		string firstNameInput = firstName.Text;
+		string lastNameInput = lastName.Text;
+		string emailInput = email.Text;
+		//Make a TryParse for exception handling
+		DateOnly dobInput = DateOnly.ParseExact(dob.Text, "yyyy/MM/dd", null);
     }
 }
