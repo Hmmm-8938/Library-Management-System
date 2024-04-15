a0d47629c5b54ce1818983a016d584fb793817ae
shannonhilland <shannonhilland@gmail.com>
2024-04-15 09:49:59 -0600

    update ViewAccount

--- Library Management System/ViewAccount.xaml.cs
+++ Library Management System/ViewAccount.xaml.cs
@@ -4,23 +4,30 @@ namespace Library_Management_System;
 
 public partial class ViewAccount : ContentPage
 {
-	public ViewAccount()
-	{
-		InitializeComponent();
-		if (AccessManager.ActiveUser != null)
-		{
+    public ViewAccount()
+    {
+        InitializeComponent();
+
+    }
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        if (AccessManager.ActiveUser != null)
+        {
             string accountFirstName = AccessManager.ActiveUser.FirstName;
-			string accountLastName = AccessManager.ActiveUser.LastName;
-			string accountCardNum = AccessManager.ActiveUser.UserID.ToString();
+            string accountLastName = AccessManager.ActiveUser.LastName;
+            string accountCardNum = AccessManager.ActiveUser.UserID.ToString();
 
             accountName.Text = $"Account: {accountFirstName} {accountLastName}";
-			accountCard.Text = $"Library Card #: {accountCardNum}";
+            accountCard.Text = $"Library Card #: {accountCardNum}";
         }
-        
+        else
+        {
+            accountName.Text = $"Account: ";
+            accountCard.Text = $"Library Card #: ";
+        }
+
 
 
     }
-
-	
-
 }
